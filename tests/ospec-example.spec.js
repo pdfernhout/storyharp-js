ac3b707595abf67e525e47e9a5f43bebbd3205f3
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-02-18 11:49:33 -0500

    fix eslint errors

--- tests/ospec-example.spec.js
+++ tests/ospec-example.spec.js
@@ -1,9 +1,9 @@
 const o = require("ospec")
 
 o("addition", function() {
-	o(1 + 1).equals(2)
+    o(1 + 1).equals(2)
 })
 
 o("subtraction", function() {
-	o(1 - 1).notEquals(2)
+    o(1 - 1).notEquals(2)
 })
