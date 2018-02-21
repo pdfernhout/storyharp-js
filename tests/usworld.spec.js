3a6fb862eedb2554eaa5a82b03885933049aea69
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-02-21 09:37:38 -0500

    add usworld test for loading session

--- tests/usworld.spec.js
+++ tests/usworld.spec.js
@@ -3,40 +3,56 @@ const fs = require("fs")
 
 const usworld = require("../dist-test/usworld")
 
+const GarTrekWorldContents = fs.readFileSync("data/GarTrek.wld").toString()
+const GarTrekSessionContents = fs.readFileSync("data/just at sphinx 2.ses").toString()
+
 o.spec("usworld", () => {
 
-    o("new", () => {
-        const world = new usworld.TWorld()
-        o(world).notEquals(null)
-    })
+    o.spec("Tworld", () => {
 
-    o("load world with missing contents", () => {
-        const world = new usworld.TWorld()
-        world.reportModeCallback = function() {}
-        try {
-            const loaded = world.loadWorldFromFileContents("")
+        o("new", () => {
+            const world = new usworld.TWorld()
+            o(world).notEquals(null)
+        })
+
+        o("load world with missing contents", () => {
+            const world = new usworld.TWorld()
+            world.reportModeCallback = function() {}
+            try {
+                const loaded = world.loadWorldFromFileContents("")
+                o(loaded).equals(true)
+            } catch (e) {
+                o(e.message).equals("Unexpected EOF loading file")
+            }
+        })
+
+        o("load world with bad contents", () => {
+            const world = new usworld.TWorld()
+            world.reportModeCallback = function() {}
+            try {
+                const loaded = world.loadWorldFromFileContents("Not a robot")
+                o(loaded).equals(true)
+            } catch (e) {
+                o(e.message).equals("File header for world file is not correct")
+            }
+        })
+
+        o("load world for GarTrek", () => {
+            const world = new usworld.TWorld()
+            world.reportModeCallback = function() {}
+            const loaded = world.loadWorldFromFileContents(GarTrekWorldContents)
             o(loaded).equals(true)
-        } catch (e) {
-            o(e.message).equals("Unexpected EOF loading file")
-        }
-    })
+        })
 
-    o("load world with bad contents", () => {
-        const world = new usworld.TWorld()
-        world.reportModeCallback = function() {}
-        try {
-            const loaded = world.loadWorldFromFileContents("Not a robot")
-            o(loaded).equals(true)
-        } catch (e) {
-            o(e.message).equals("File header for world file is not correct")
-        }
-    })
-
-    o("load world for GarTrek", () => {
-        const world = new usworld.TWorld()
-        world.reportModeCallback = function() {}
-        const contents = fs.readFileSync("data/GarTrek.wld").toString()
-        const loaded = world.loadWorldFromFileContents(contents)
-        o(loaded).equals(true)
+        o("load session for GarTrek", () => {
+            const world = new usworld.TWorld()
+            world.reportModeCallback = function() {}
+            const loadedWorld = world.loadWorldFromFileContents(GarTrekWorldContents)
+            o(loadedWorld).equals(true)
+            o(world.focus).equals(null)
+            const loadedSession = world.loadSessionFromFile("GarTrek.wld", GarTrekSessionContents)
+            o(loadedSession).equals(true)
+            o(world.focus.phrase).equals("<sphinx>")
+        })  
     })
 })
