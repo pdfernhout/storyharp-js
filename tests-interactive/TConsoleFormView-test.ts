5fab2e6cc54fb494ad9836617b631e0e50686ddc
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-02-24 12:16:10 -0500

    Add interactive test to display TConsoleFormView

---
+++ tests-interactive/TConsoleFormView-test.ts
@@ -0,0 +1,7 @@
+
+// Exported from: ../../storyharp-js/converted_source/uscontextwizard.lfm
+import * as m from "mithril"
+
+import { viewConsoleForm } from "../source/TConsoleFormView"
+
+m.mount(document.body, { view: viewConsoleForm })
