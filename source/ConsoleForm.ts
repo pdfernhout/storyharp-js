ed8a90be2549cc97bda10d0ad1f61c51e1f7d71c
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-02-24 18:18:53 -0500

    Make very simple test console work

--- source/ConsoleForm.ts
+++ source/ConsoleForm.ts
@@ -1,5 +1,48 @@
 import * as m from "mithril"
 
+import { TWorld } from "./TWorld"
+
+function availableCommands(world: TWorld): string[] {
+    const result: string[] = []
+
+    for (let rule of world.rules) {
+        if (rule.available) {
+            if (rule.command.phrase === "") {
+                continue
+            }
+            const command = rule.command.phrase
+            /*
+            if ((UNRESOLVED.pos("$", rule.command.phrase) === 1)) {
+                // only list first riddle answer
+                command = firstRiddleAnswer
+            }
+            */
+            if (result.indexOf(command) === -1) {
+                result.push(command)
+            }
+        }
+    }
+    return result
+}
+
+function doCommand(domain: any, command: string) {
+    console.log("doCommand", command)
+    domain.commandList.doCommandPhrase(domain.consoleForm, domain.ruleEditorForm, command)
+}
+
+function viewChoices(domain: any) {
+    const commands = availableCommands(domain.world)
+    return m("div", 
+        m("hr"),
+        m("div", "You can choose from:"),
+        commands.sort().map(command => m("div.ma2", { onclick: () => doCommand(domain, command) }, command))
+    )
+}
+
 export function viewConsoleForm(domain: any) {
-    return m(".ConsoleForm", "Console Form", JSON.stringify(domain))
+    return m(".ConsoleForm",
+        m("h1", "StoryHarp-js"),
+        domain.transcript.map((text: string) => m("div.mw6", text)),
+        viewChoices(domain)
+    )
 }
--- tests-interactive/ConsoleForm-test.ts
+++ tests-interactive/ConsoleForm-test.ts
@@ -4,10 +4,52 @@ import * as m from "mithril"
 
 import { viewConsoleForm } from "../source/ConsoleForm"
 
+import { TWorld } from "../source/TWorld"
+import { TSCommandList } from "../source/TSCommandList";
+
+const world = new TWorld()
+const commandList = new TSCommandList(world)
+const transcript = [
+    "Hello, StoryHarp!",
+    "Welcome to an Audioventure"
+]
+
 const fakeDomain = {
-    test: "hello"
+    world,
+    commandList,
+    transcript,
+    consoleForm: {
+        speechSystem: {
+            lastSaidTextWithMacros: "TEST",
+            stripMacros: (text: string) => text,
+            sayTextWithMacros: (text: string) => null,
+            listenForAvailableCommands: () => null,
+            checkForSayOptionsMacro: () => null,
+            speakText: (text: string) => null,
+        },
+        addLineToTranscript: (text: string) => transcript.push(text),
+        scrollTranscriptEndIntoView: () => null,
+        updateVariables: () => null,
+        VariablesListBox: {
+            Invalidate: () => null
+        }
+    },
+    ruleEditorForm: {}
 }
 
 const MyComponent = { view: () => viewConsoleForm(fakeDomain) }
 
-m.mount(document.body, MyComponent)
+async function loadTestWorld() {
+    const GarTrekWorldContents = await m.request("../data/GarTrek.wld", {deserialize: (text) => text})
+
+    fakeDomain.world.reportModeCallback = function() {}
+
+    const loaded = fakeDomain.world.loadWorldFromFileContents(GarTrekWorldContents)
+    if (!loaded) throw new Error("Failed to load")
+
+    fakeDomain.world.newSession()
+
+    m.mount(document.body, MyComponent)
+}
+
+loadTestWorld()
