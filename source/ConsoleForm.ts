1707f75f97225ef6c0f318d64366cc49f604f75d
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-02-24 19:23:02 -0500

    add better support for riddles in ConsoleForm

--- source/ConsoleForm.ts
+++ source/ConsoleForm.ts
@@ -1,8 +1,11 @@
 import * as m from "mithril"
 
+import { Color } from "./common"
 import { TWorld } from "./TWorld"
 
-function availableCommands(world: TWorld): string[] {
+const firstRiddleAnswer = "say an answer for a riddle"
+
+function availableCommands(world: TWorld, showRiddleAnswers=false): string[] {
     const result: string[] = []
 
     for (let rule of world.rules) {
@@ -10,13 +13,11 @@ function availableCommands(world: TWorld): string[] {
             if (rule.command.phrase === "") {
                 continue
             }
-            const command = rule.command.phrase
-            /*
-            if ((UNRESOLVED.pos("$", rule.command.phrase) === 1)) {
+            let command = rule.command.phrase
+            if (!showRiddleAnswers && rule.command.phrase.startsWith("$")) {
                 // only list first riddle answer
                 command = firstRiddleAnswer
             }
-            */
             if (result.indexOf(command) === -1) {
                 result.push(command)
             }
@@ -25,9 +26,32 @@ function availableCommands(world: TWorld): string[] {
     return result
 }
 
-function doCommand(domain: any, command: string) {
-    console.log("doCommand", command)
-    domain.commandList.doCommandPhrase(domain.consoleForm, domain.ruleEditorForm, command)
+function doCommand(domain: any, commandPhrase: string) {
+    console.log("doCommand", commandPhrase)
+    if (commandPhrase === firstRiddleAnswer) {
+        // for riddles - need to be reassembled into command string first
+        const answer = prompt("Please enter the answer to a riddle. [case-sensitive]", "")
+        if (!answer) return
+        commandPhrase = "answer " + answer
+    }
+    let commandPhraseModified
+    if ((availableCommands(domain.world, true).indexOf(commandPhrase) === -1)) {
+        commandPhraseModified = "$" + commandPhrase
+    } else {
+        commandPhraseModified = commandPhrase
+    }
+    console.log("test", commandPhraseModified, availableCommands(domain.world, true))
+    if ((availableCommands(domain.world, true).indexOf(commandPhraseModified) === -1)) {
+        if (commandPhrase.startsWith("$")) {
+            // elimitate leading $
+            commandPhrase = commandPhrase.substring(1)
+        }
+        domain.consoleForm.addLineToTranscript("> " + commandPhrase, Color.clRed);
+        // bug - or bad riddle answer.
+        domain.consoleForm.addLineToTranscript("That accomplishes nothing.", Color.clBlue)
+        return
+    }   
+    domain.commandList.doCommandPhrase(domain.consoleForm, domain.ruleEditorForm, commandPhraseModified)
 }
 
 function viewChoices(domain: any) {
