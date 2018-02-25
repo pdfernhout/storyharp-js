de29408aa9c0788f03745831bd4dd0539adb28fd
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-02-25 12:58:19 -0500

    Delete TConsoleForm and TConsoleFormView and interactive test
    Some of the code about handling options or files may still be useful.
    The generated code can be consulted for that.

--- tests-interactive/TConsoleFormView-test.ts
+++
@@ -1,7 +0,0 @@
-
-// Exported from: ../../storyharp-js/converted_source/uscontextwizard.lfm
-import * as m from "mithril"
-
-import { viewConsoleForm } from "../source/TConsoleFormView"
-
-m.mount(document.body, { view: viewConsoleForm })
