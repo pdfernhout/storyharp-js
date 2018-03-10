ae7f7193bb113dd0d53dbed69f4f0385c409fbcd
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2018-03-10 10:44:55 -0500

    Improve combobox interactive test

--- tests-interactive/TQuickFillComboBox-test.ts
+++ tests-interactive/TQuickFillComboBox-test.ts
@@ -11,31 +11,34 @@ const editableList = [
     "sassafras",
     "sarsaparilla",
     "more",
+    "a very long choice that is impractical",
+    "a very long choice that is really impractical and goes on and on and on and on and on and on and on and on",
 ]
 
-let editableValue: "hello"
+let editableValue = "hello"
+let otherValue = ""
 
 const MyComponent = { 
     view: () => m("div.ma3", 
-        "This should be a TQuickFillComboBox:",
-        m(TQuickFillComboBox, <any>{
-            items: ["a", "b", "c", "sassafras", "a very long choice that is impactical",  "a very long choice that is really impactical and goes on and on and on and on and on and on and on and on", "d"]
-        }),
-
-        m("div.ma3", "This is a more advanced test:"),
+        m("div", "Combobox test:"),
         m(TQuickFillComboBox, <any>{
             value: editableValue,
             items: editableList,
             onchange: (event: any) => {
-                console.log("onchange", event)
+                console.log("onchange from combobox", event)
                 editableValue = event.target.value
             },
         }),
-        m("div.ma2", "The combobox value is: ", editableValue),
-        "Which can also be changed here:",
+        m("div.mt7", "The combobox value is: \"" + editableValue + "\"", ),
+        m("div", "Which can also be changed here:"),
         m("input", {
             value: editableValue,
             onchange: (event: any) => { editableValue = event.target.value },
+        }),
+        m("div.mt3", "Unrelated input for focus shift testing:"),
+        m("input", {
+            value: otherValue,
+            onchange: (event: any) => { otherValue = event.target.value },
         })
     )
 }
